43544284598cc5db2ba80ef00bd4d1df8ba16ffa
Paresh Patel <70636561+Pareshpatel90@users.noreply.github.com>
2023-11-07 10:28:49 +0530

    Update create-custom-posttype.php

--- create-custom-posttype.php
+++ create-custom-posttype.php
@@ -1,1 +1,22 @@
-
+<?php 
+/**
+*We can create custom post type using this function. we can create mutiple custome post type using this function. 
+*In this example we have created one custome post type services.
+**/
+function create_posttype() {
+  //custom posttype service code start
+	register_post_type('services',
+	array(
+	  'labels' => array(
+	   'name' => __( 'Services' ),
+	   'singular_name' => __( 'Service' ),
+	  ),
+	'supports'=> array( 'title', 'editor', 'thumbnail'),
+	'public' => true,
+	'menu_icon'=>'dashicons-chart-pie',
+	'has_archive' => false,
+	'rewrite' => array('slug' => 'services'),
+	));
+  //code end
+}
+add_action( 'init', 'create_posttype' 
