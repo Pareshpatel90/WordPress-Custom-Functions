375c334c4fe46132a81198879cb1b89282a4fa19
Paresh Patel <70636561+Pareshpatel90@users.noreply.github.com>
2023-11-07 16:56:59 +0530

    Create create-widgets.php

---
+++ create-widgets.php
@@ -0,0 +1,21 @@
+<?php 
+/* Register the widgets */
+add_action( 'widgets_init', 'mytheme_widgets_init');
+function mytheme_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Primary Sidebar', 'mytheme' ),
+		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => __( 'Secondary Sidebar', 'mytheme' ),
+		'id'            => 'sidebar-2',
+		'before_widget' => '<ul><li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li></ul>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+}
